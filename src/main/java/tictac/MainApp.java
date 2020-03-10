51a85a027fdb15e55e32282055eaae1c8bf2aead
Jansmig <jan.smigielski@gmail.com>
2020-03-10 18:23:26 +0100

    7 - first complete version, non-OOP

--- src/main/java/tictac/MainApp.java
+++ src/main/java/tictac/MainApp.java
@@ -1,7 +1,11 @@
 package tictac;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
@@ -9,6 +13,9 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -56,39 +63,17 @@ public class MainApp extends Application {
             System.out.println("O player's turn");
         }
     }
-/*
-    public static boolean scanForX (GridPane gridpane, int col, int row) {
+
+    // for later usage:
+    public static Node getNode (GridPane gridpane, int col, int row) {
 
         for (Node node : gridpane.getChildren()) {
             if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
-                if(node.toString().contains("X"));
-                }
+                return node;
             }
-        return false;
-    }
-
- */
-
-    public static boolean scanForX (GridPane gridpane) {
-        return gridpane.getChildren().get(0).toString().contains("X");
-    }
-
-    public static String scanForIdentifier (GridPane gridpane) {
-        return
-        gridpane.getChildren().get(0).toString();
-    }
-
-    /*
-    public boolean chceckRow0() {
-        if (tile00.isX() && tile10.isX() && tile20.isX()) {
-            return true;
-        } else {
-            return false;
         }
+        return null;
     }
-
-     */
-
 
 
 
@@ -96,18 +81,20 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         GridPane gameboard = new GridPane();
-        gameboard.setPrefSize(TILE_SIZE * 3, TILE_SIZE * 3);
-        gameboard.setGridLinesVisible(true);
+        gameboard.setPrefSize(TILE_SIZE * 3, TILE_SIZE * 4);
+        gameboard.setGridLinesVisible(false);
+        gameboard.setStyle("-fx-background-color: linear-gradient(#23cdea, rgba(8,32,55,0.84))");
 
         for (int i = 0; i < 3; i++ ) {
             ColumnConstraints col = new ColumnConstraints(TILE_SIZE);
             gameboard.getColumnConstraints().add(col);
         }
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < 4; j++) {
             RowConstraints row = new RowConstraints(TILE_SIZE);
             gameboard.getRowConstraints().add(row);
         }
+
 
         //row 0
         Tile tile00 = new Tile(0,0,1,1);
@@ -139,6 +126,19 @@ public class MainApp extends Application {
         Tile tile22 = new Tile(2,2,1,1);
         gameboard.add(tile22, (int) tile22.getX(), (int) tile22.getY());
 
+        Text xScore = new Text("X score: " + xVictoryCounter);
+        xScore.setFont(Font.font("Agency FB", FontWeight.BOLD, 48));
+        GridPane.setHalignment(xScore, HPos.CENTER);
+        GridPane.setValignment(xScore, VPos.TOP);
+        xScore.setFill(Color.WHITE);
+        gameboard.add(xScore, 1, 3);
+
+        Text oScore = new Text("O score: " + oVictoryCounter);
+        oScore.setFont(Font.font("Agency FB", FontWeight.BOLD, 48));
+        GridPane.setHalignment(oScore, HPos.CENTER);
+        GridPane.setValignment(oScore, VPos.BOTTOM);
+        oScore.setFill(Color.WHITE);
+        gameboard.add(oScore, 1, 3);
 
 
         gameboard.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -153,8 +153,19 @@ public class MainApp extends Application {
                         (tile00.isX() && tile11.isX() && tile22.isX()) ||
                         (tile20.isX() && tile11.isX() && tile02.isX())) {
                     System.out.println("X player won!");
-                    gameboard.getChildren().clear();
                     xVictoryCounter++;
+                    xScore.setText("X score: " + xVictoryCounter);
+                    tile00.tileReset();
+                    tile10.tileReset();
+                    tile20.tileReset();
+                    tile01.tileReset();
+                    tile11.tileReset();
+                    tile21.tileReset();
+                    tile02.tileReset();
+                    tile12.tileReset();
+                    tile22.tileReset();
+
+
                 } else if ((tile00.isO() && tile10.isO() && tile20.isO()) ||
                         (tile01.isO() && tile11.isO() && tile21.isO()) ||
                         (tile02.isO() && tile12.isO() && tile22.isO()) ||
@@ -164,20 +175,35 @@ public class MainApp extends Application {
                         (tile00.isO() && tile11.isO() && tile22.isO()) ||
                         (tile20.isO() && tile11.isO() && tile02.isO())) {
                     System.out.println("O player won!");
-                    gameboard.getChildren().clear();
                     oVictoryCounter++;
+                    oScore.setText("O score: " + oVictoryCounter);
+                    tile00.tileReset();
+                    tile10.tileReset();
+                    tile20.tileReset();
+                    tile01.tileReset();
+                    tile11.tileReset();
+                    tile21.tileReset();
+                    tile02.tileReset();
+                    tile12.tileReset();
+                    tile22.tileReset();
+
+
                 } else if (!tile00.playable && !tile10.playable && !tile20.playable &&
                         !tile01.playable && !tile11.playable && !tile21.playable &&
                         !tile02.playable && !tile12.playable && !tile22.playable) {
                     System.out.println("Draw!");
-                    gameboard.getChildren().clear();
+                    tile00.tileReset();
+                    tile10.tileReset();
+                    tile20.tileReset();
+                    tile01.tileReset();
+                    tile11.tileReset();
+                    tile21.tileReset();
+                    tile02.tileReset();
+                    tile12.tileReset();
+                    tile22.tileReset();
                 }
             }
         });
-
-        Button newGame = new Button();
-        newGame.setText("New Game");
-
 
 
         Scene scene = new Scene (gameboard, TILE_SIZE * 3, TILE_SIZE * 4, Color.DARKSLATEGREY);
@@ -191,13 +217,40 @@ public class MainApp extends Application {
         announceTurn();
 
 
+        /*
+        // this button is Useless in the current state
+
+        Button newGame = new Button();
+        newGame.setText("New Game");
+        GridPane.setHalignment(newGame, HPos.CENTER);
+        GridPane.setHalignment(newGame, HPos.CENTER);
+        gameboard.add(newGame, 0, 3);
+        newGame.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                 if (!tile00.playable && !tile10.playable && !tile20.playable &&
+                        !tile01.playable && !tile11.playable && !tile21.playable &&
+                        !tile02.playable && !tile12.playable && !tile22.playable) {
+                    tile00.tileReset();
+                    tile10.tileReset();
+                    tile20.tileReset();
+                    tile01.tileReset();
+                    tile11.tileReset();
+                    tile21.tileReset();
+                    tile02.tileReset();
+                    tile12.tileReset();
+                    tile22.tileReset();
+
+                } else {
+                    System.out.println("Please finish the current game first.");
+                }
+            }
+        });
+
+         */
+
     }
 
-/*
-        // reset
-        ObservableList<Node> children = gameboard.getChildren();
-        children.clear();
- */
 
     // Calling Platform.exit() is the preferred way to explicitly terminate a JavaFX Application
 
